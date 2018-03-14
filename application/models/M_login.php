0f9013eb5e638f08d5d6b92b278edfb2975d2e4c
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-14 16:31:39 -0500

    cambios en el login

---
+++ application/models/M_login.php
@@ -0,0 +1,25 @@
+<?php
+
+class M_login extends  CI_Model{
+    function __construct(){
+        parent::__construct();
+    }
+
+    function insertarDatos($arrayInsert, $tabla){
+        $this->db->insert($tabla, $arrayInsert);
+        $sol = $this->db->insert_id();
+        if($this->db->affected_rows() != 1) {
+            throw new Exception('Error al insertar');
+            $data['error'] = EXIT_ERROR;
+        }
+        return array("error" => EXIT_SUCCESS, "msj" => MSJ_INS, "Id" => $sol);
+    }
+
+    function verificarUsuario($user){
+        $sql = "SELECT *
+                  FROM users
+                 WHERE Email LIKE '%".$user."%'";
+        $result = $this->db->query($sql);
+        return $result->result();
+    }
+}
