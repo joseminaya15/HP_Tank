6879b4a27d3451e0140d7666d2ff8410960d5c4c
joseminaya15 <jose.minayac15@gmail.com>
2018-03-14 16:02:37 -0500

    login

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,29 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+//	RECURSOS PUBLICOS
+defined('RUTA_CSS')     OR define('RUTA_CSS'    , 'http://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null).'/HP_Tank/public/css/');
+defined('RUTA_FONTS')   OR define('RUTA_FONTS'  , 'http://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null).'/HP_Tank/public/fonts/');
+defined('RUTA_IMG')     OR define('RUTA_IMG'    , 'http://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null).'/HP_Tank/public/img/');
+defined('RUTA_JS')      OR define('RUTA_JS'     , 'http://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null).'/HP_Tank/public/js/');
+defined('RUTA_PLUGINS') OR define('RUTA_PLUGINS', 'http://'.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null).'/HP_Tank/public/plugins/');
+
+//NOMBRE DEL PROYECTO
+defined('PROYECTO_NAME') OR define('PROYECTO_NAME','SAP_gana_goleada');
+
+//MENSAJES DE ERROR,CONFIRMACION Y EDICION
+defined('MSJ_INS') OR define('MSJ_INS', 'Se Registr&oacute; Correctamente');
+defined('MSJ_UPT') OR define('MSJ_UPT', 'Se Edit&oacute; Correctamente');
+defined('MSJ_DEL') OR define('MSJ_DEL', 'Se Elimin&oacute; Correctamente');
+defined('MSJ_ANL') OR define('MSJ_ANL', 'Se Anul&oacute; Correctamente');
+defined('MSJ_GEN') OR define('MSJ_GEN', 'Se Gener&oacute; Correctamente');
+defined('MSJ_INSERT_ERROR')   OR define('MSJ_INSERT_ERROR', 'Se ha insertado incorrectamente');
+defined('MSJ_INSERT_SUCCESS') OR define('MSJ_INSERT_SUCCESS', 'Se ha insertado correctamente');
+defined('MSJ_DELETE_ERROR')   OR define('MSJ_DELETE_ERROR', 'Se ha eliminado incorrectamente');
+defined('MSJ_DELETE_SUCCESS') OR define('MSJ_DELETE_SUCCESS', 'Se ha eliminado correctamente');
+
+//ESTADOS
+defined('FLAG_PENDIENTE') OR define('FLAG_PENDIENTE', 1);
+defined('FLAG_APROBADO')  OR define('FLAG_APROBADO', 2);
+defined('FLAG_RECHAZADO') OR define('FLAG_RECHAZADO', 3);
---
+++ application/controllers/Registro.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Registro extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('v_registro');
+	}
+}
