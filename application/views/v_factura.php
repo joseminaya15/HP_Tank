84ab7906011afc5d5aa09f133bd9e00ff2ed7598
joseminaya15 <jose.minayac15@gmail.com>
2018-03-15 11:55:47 -0500

    arreglos

---
+++ application/views/v_factura.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible"  content="IE=edge">
+        <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta name="description"            content="HP Super Vendendor">
+        <meta name="keywords"               content="HP Super Vendendor">
+        <meta name="robots"                 content="Index,Follow">
+        <meta name="date"                   content="Febrero 15, 2018"/>
+        <meta name="language"               content="es">
+        <meta name="theme-color"            content="#000000">
+        <title>HP Super Vendendor</title>
+        <link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/favicon.png">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>mdl/material.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>datetimepicker/css/bootstrap-material-datetimepicker.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>font-awesome.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>material-icons.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>roboto.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
+        <link rel="stylesheet"    href="<?php echo RUTA_CSS?>index.css?v=<?php echo time();?>">
+    </head>
+    <body>
+        <section id="login">
+            <video autoplay loop muted>
+                <source src="<?php echo RUTA_VIDEO?>video_hp.mp4">
+            </video>
+            <div class="header">
+                <div class="header-left">
+                    <img class="logo-header" src="<?php echo RUTA_IMG?>logos/favicon.png">
+                    <h2>Programa de incentivos DeskJet HP Tank</h2>
+                </div>
+            </div>
+            <div class="container mdl-card-container factura">
+                <div class="center">
+                    <div class="text-right">
+                        <h2 class="person_name">Bienvenido(a) <?php echo $nombre == null ? '' : $nombre; ?></h2>
+                        <p class="team_name">Canal <?php echo $canal == null ? '' : $canal; ?></p>
+                    </div>
+                    <div id="Nueva_anotacion" class="mdl-card mdl-card-menu" onclick="goToMenu(this.id)">
+                        <div>
+                            <p>Total ganado hasta hoy</p>
+                            <div>
+                                <img src="">
+                                <h2>997</h2>
+                            </div>
+                            <a>Descargar factura cargada<i class="mdi mdi-play_arrow"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <p>&copy; Copyright 2018 Hewlett-Packard Development Company, L.P.</p>
+            </div>
+        </section>
+
+        <script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap/bootstrap.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>moment/moment.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>datetimepicker/js/bootstrap-material-datetimepicker.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>jquery-mask/jquery.mask.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>Utils.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jsmenu.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jslogin.js?v=<?php echo time();?>"></script>
+        <script type="text/javascript">
+            if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+                $('select').selectpicker('mobile');
+            } else {
+                $('select').selectpicker();
+            }
+        </script>
+    </body>
+</html>
