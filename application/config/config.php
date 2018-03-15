408c702bda9d570cfcc9301615ee5277e3ac04dc
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-03-15 12:02:31 -0500

    ruta del login a factura

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/HP_Tank/';
-$config['base_url'] = 'http://192.168.200.163/HP_Tank/';
+$config['base_url'] = 'http://localhost/HP_Tank/';
 
 /*
 |--------------------------------------------------------------------------
