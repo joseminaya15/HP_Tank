a60c7bc837e4e9c24655234243addab71d5baa9f
joseminaya15 <jose.minayac15@gmail.com>
2018-03-16 14:49:05 -0500

    nada

--- public/js/jsfactura.js
+++ public/js/jsfactura.js
@@ -122,5 +122,5 @@ function selectPrint(){
 function readCount(){
   var read       = $('#cantidad').val();
   var Monto      = $('#monto').val(read*numberSpiff);
-  numberCantidad = read;
+  numberCantidad = read; 
 }
